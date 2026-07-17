4a6ed6b8aebb71ced44dd7e1861f780006b2fe84
SiddhiPandey08 <smpd2708@gmail.com>
2026-07-17 14:27:52 +0530

    Resetting rate limiter

--- backend/middlewares/rateLimiter.js
+++ backend/middlewares/rateLimiter.js
@@ -6,13 +6,13 @@ dotenv.config();
 const maxRequests = process.env.NODE_ENV === "production" ? 100 : 1000;
 
 export const globalLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 5 * 60 * 1000,
   max: maxRequests,
   message: { message: "Too many requests, please try again later" },
 });
 
 export const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: process.env.NODE_ENV === "production" ? 20 : 100, // stricter for auth
+  windowMs: 5 * 60 * 1000,
+  max: process.env.NODE_ENV === "production" ? 50 : 100, // stricter for auth
   message: { message: "Too many auth attempts, please try again later" },
 });
